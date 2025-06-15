0be054b7208fd7f8c8156ccb37aae67344309e87
carlosjosuemorales9 <ma23009@ues.edu.sv>
2025-06-15 12:26:21 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+# Sistema de Venta y Control Escolar
+
+def main():
+    print("Bienvenido al Sistema de Control Escolar")
+    # Aquí iría la lógica principal
+
+if __name__ == "__main__":
+    main()
